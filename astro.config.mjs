69a50eaf67e7e464b19d370ac575468cff5fabf1
Simon Wiles <simonjwiles@gmail.com>
2022-10-13 15:30:46 -0700

    Add svelte infrastructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
+  integrations: [svelte()],
   vite: {
     build: {
       rollupOptions: {
